3473951497e8848836f48809a9e44e37b1cfe9b0
zhuchane <zhuchane@126.com>
2019-10-16 15:10:33 +0800

    add ts doc

---
+++ tf2.0/tf2imp.py
@@ -0,0 +1,28 @@
+
+#!/usr/bin/env python
+# encoding: utf-8
+
+#set( $SITE = "https://github.com/zhuchance/Aimusic" )
+
+"""
+@version: 
+@author: zhuqq
+@license: Apache Licence 
+@contact: couchance@gmail.com
+@site: none
+@software: ${PRODUCT_NAME}
+@file: tf2.py
+@time: 2019/10/16
+"""
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
